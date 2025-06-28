b836c31eb70efb008ff9669052f0a004b57a27b8
ivangol739 <ivangol739@gmail.com>
2025-06-28 23:56:31 +0300

    разграничены права доступа

--- app/routers/bookings.py
+++ app/routers/bookings.py
@@ -9,6 +9,8 @@ from app.models.bookings import Booking
 from app.models.flight import Flight
 from slugify import slugify
 from datetime import datetime
+from app.routers.auth import get_current_user
+
 
 router = APIRouter(prefix='/bookings', tags=['bookings'])
 
@@ -18,31 +20,36 @@ async def all_bookings(db: Annotated[AsyncSession, Depends(get_db)]):
     return bookings.all()
 
 @router.post('/')
-async def create_booking(db: Annotated[AsyncSession, Depends(get_db)], booking_data: BookingCreate, user_id: int=1):
-    flight = await db.scalar(select(Flight).where(Flight.id == booking_data.flight_id))
-    if flight is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
-    if flight.available_seats < booking_data.seats:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
+async def create_booking(db: Annotated[AsyncSession, Depends(get_db)], booking_data: BookingCreate, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin') or get_user.get('is_supplier'):
+        flight = await db.scalar(select(Flight).where(Flight.id == booking_data.flight_id))
+        if flight is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
+        if flight.available_seats < booking_data.seats:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
 
-    slug = slugify(f"booking-{user_id}-{flight.id}-{datetime.utcnow().timestamp()}")
+        slug = slugify(f"booking-{get_user.get('id')}-{flight.id}-{datetime.utcnow().timestamp()}")
 
+        await db.execute(insert(Booking).values(
+            user_id=get_user.get('id'),
+            flight_id=booking_data.flight_id,
+            seats=booking_data.seats,
+            slug=slug,
+            status="confirmed",
+        ))
 
-    await db.execute(insert(Booking).values(
-        user_id=user_id,
-        flight_id=booking_data.flight_id,
-        seats=booking_data.seats,
-        slug=slug,
-        status="confirmed",
-    ))
+        flight.available_seats -= booking_data.seats
 
-    flight.available_seats -= booking_data.seats
-
-    await db.commit()
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
+        await db.commit()
+        return {
+            'status_code': status.HTTP_201_CREATED,
+            'transaction': 'Successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You have not enough permission for this action'
+        )
 
 
 @router.get('/{flight_slug}')
@@ -64,54 +71,67 @@ async def booking_detail(db: Annotated[AsyncSession, Depends(get_db)] ,booking_s
     return booking
 
 @router.put('/{booking_slug}')
-async def update_booking(db: Annotated[AsyncSession, Depends(get_db)], update_data: BookingUpdate, booking_slug: str):
-    booking = await db.scalar(select(Booking).where(Booking.slug == booking_slug))
-    if booking is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
+async def update_booking(db: Annotated[AsyncSession, Depends(get_db)], update_data: BookingUpdate, booking_slug: str, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin') or get_user.get('is_supplier'):
+        booking = await db.scalar(select(Booking).where(Booking.slug == booking_slug))
+        if booking is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
 
-    flight = await db.scalar(select(Flight).where(Flight.id == booking.flight_id))
-    if flight is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
+        flight = await db.scalar(select(Flight).where(Flight.id == booking.flight_id))
+        if flight is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
 
-    # Обновляем количество мест, если передано
-    if update_data.seats is not None:
-        seats_diff = update_data.seats - booking.seats
-        if flight.available_seats < seats_diff:
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
+        # Обновляем количество мест, если передано
+        if update_data.seats is not None:
+            seats_diff = update_data.seats - booking.seats
+            if flight.available_seats < seats_diff:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
 
-        flight.available_seats -= seats_diff
-        booking.seats = update_data.seats
+            flight.available_seats -= seats_diff
+            booking.seats = update_data.seats
 
-    # Обновляем статус, если передан
-    if update_data.status is not None:
-        if booking.status != update_data.status:
-            # Если отменяем — возвращаем места
-            if update_data.status == "canceled" and booking.status != "canceled":
-                flight.available_seats += booking.seats
-            # Если меняем обратно на confirmed — проверяем доступность мест
-            if update_data.status == "confirmed" and booking.status == "canceled":
-                if flight.available_seats < booking.seats:
-                    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
-                flight.available_seats -= booking.seats
-            booking.status = update_data.status
+        # Обновляем статус, если передан
+        if update_data.status is not None:
+            if booking.status != update_data.status:
+                # Если отменяем — возвращаем места
+                if update_data.status == "canceled" and booking.status != "canceled":
+                    flight.available_seats += booking.seats
+                # Если меняем обратно на confirmed — проверяем доступность мест
+                if update_data.status == "confirmed" and booking.status == "canceled":
+                    if flight.available_seats < booking.seats:
+                        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Not enough seats available')
+                    flight.available_seats -= booking.seats
+                booking.status = update_data.status
 
-    db.add_all([booking, flight])
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Booking update is successful'
-    }
+        db.add_all([booking, flight])
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Booking update is successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You have not enough permission for this action'
+        )
 
-@router.delete('/')
-async def delete_booking(db: Annotated[AsyncSession, Depends(get_db)], booking_id: int):
-    booking = await db.get(Booking, booking_id)
-    if booking is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
-    flight = await db.get(Flight, booking.flight_id)
-    flight.available_seats += booking.seats
-    await db.delete(booking)
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Booking delete is successful'
-    }
+
+@router.delete('/{booking_slug}')
+async def delete_booking(db: Annotated[AsyncSession, Depends(get_db)], booking_id: int, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin') or get_user.get('is_supplier'):
+        booking = await db.get(Booking, booking_id)
+        if booking is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Booking not found')
+        flight = await db.get(Flight, booking.flight_id)
+        flight.available_seats += booking.seats
+        await db.delete(booking)
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Booking delete is successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You have not enough permission for this action'
+        )
--- app/routers/flight.py
+++ app/routers/flight.py
@@ -10,6 +10,8 @@ from sqlalchemy import select
 
 from slugify import slugify
 from datetime import datetime
+from app.routers.auth import get_current_user
+
 
 router = APIRouter(prefix='/flights', tags=['flights'])
 
@@ -19,49 +21,66 @@ async def get_all_flights(db: Annotated[AsyncSession, Depends(get_db)]):
     return flights.all()
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-async def create_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_data: FlightCreate):
-    await db.execute(insert(Flight).values(
-        flight_number=flight_data.flight_number,
-        departure_city=flight_data.departure_city,
-        arrival_city=flight_data.arrival_city,
-        departure_time=flight_data.departure_time,
-        arrival_time=flight_data.arrival_time,
-        price=flight_data.price,
-        available_seats=flight_data.available_seats,
-        slug=slugify(flight_data.flight_number),
-    ))
-
-    await db.commit()
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
+async def create_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_data: FlightCreate, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        await db.execute(insert(Flight).values(
+            flight_number=flight_data.flight_number,
+            departure_city=flight_data.departure_city,
+            arrival_city=flight_data.arrival_city,
+            departure_time=flight_data.departure_time,
+            arrival_time=flight_data.arrival_time,
+            price=flight_data.price,
+            available_seats=flight_data.available_seats,
+            slug=slugify(flight_data.flight_number),
+        ))
+        await db.commit()
+        return {
+            'status_code': status.HTTP_201_CREATED,
+            'transaction': 'Successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You must be admin user for this'
+        )
 
 @router.put('/{flight_slug}')
-async def update_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_slug: str, flight_data: FlightUpdate):
-    flight = await db.scalar(select(Flight).where(Flight.slug == flight_slug))
-    if flight is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
+async def update_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_slug: str, flight_data: FlightUpdate, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        flight = await db.scalar(select(Flight).where(Flight.slug == flight_slug))
+        if flight is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
 
-    flight.price = flight_data.price
-    flight.available_seats = flight_data.available_seats
+        flight.price = flight_data.price
+        flight.available_seats = flight_data.available_seats
 
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Flight update is successful'
-    }
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Flight update is successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You must be admin user for this'
+        )
 
 
 @router.delete('/{flight_slug}')
-async def delete_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_slug: str):
-    flight = await db.scalar(select(Flight).where(Flight.slug == flight_slug))
-    if flight is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
+async def delete_flight(db: Annotated[AsyncSession, Depends(get_db)], flight_slug: str, get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        flight = await db.scalar(select(Flight).where(Flight.slug == flight_slug))
+        if flight is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Flight not found')
 
-    await db.delete(flight)
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Flight delete is successful'
-    }
+        await db.delete(flight)
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Flight delete is successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail='You must be admin user for this'
+        )
